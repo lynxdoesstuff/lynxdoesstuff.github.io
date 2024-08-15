487d3d146ae137bf91b529e72385ce9bf8264aac
lynx <129530932+lynxdoesstuff@users.noreply.github.com>
2024-08-14 19:26:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 const particles = [];
-
 const mouse = {
   x: null,
   y: null,
 };
-
 window.addEventListener('mousemove', (event) => {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
 });
-
 class Particle {
   constructor(x, y, size, color, directionX, directionY) {
     this.x = x;
@@ -27,7 +22,6 @@ class Particle {
     this.directionY = directionY;
     this.opacity = 1;
   }
-
   draw() {
     ctx.fillStyle = this.color;
     ctx.beginPath();
@@ -35,18 +29,29 @@ class Particle {
     ctx.closePath();
     ctx.fill();
   }
-
   update() {
-    // Bounce off the edges of the canvas
-    if (this.x + this.size > canvas.width || this.x - this.size < 0) {
-      this.directionX = -this.directionX;
-    }
-    if (this.y + this.size > canvas.height || this.y - this.size < 0) {
-      this.directionY = -this.directionY;
-    }
     this.x += this.directionX;
     this.y += this.directionY;
-    // Add a console.log statement to track the particle's position
-    console.log(`Particle x: ${this.x}, y: ${this.y}`);
+    this.draw();
   }
 }
+function init() {
+  for (let i = 0; i < 100; i++) {
+    let size = Math.random() * 5 + 1;
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    let directionX = Math.random() * 2 - 1;
+    let directionY = Math.random() * 2 - 1;
+    let color = 'white';
+    particles.push(new Particle(x, y, size, color, directionX, directionY));
+  }
+}
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+  }
+}
+init();
+animate();
